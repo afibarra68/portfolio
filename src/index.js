71159fa65a5fd5d5732340c7451e6222411da3ca
zfarmingdev1 <zfarming.dev1@gmail.com>
2021-09-25 23:26:13 -0500

    new staged changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <script
+      src="https://unpkg.com/react/umd/react.production.min.js"
+      crossorigin
+    ></script>
+    <script
+      src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+      crossorigin
+    ></script>
+    <script
+      src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+      crossorigin
+    ></script>
+
+    <script>var Alert = ReactBootstrap.Alert;</script>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/me.js
@@ -0,0 +1,12 @@
+import { Col, Container } from "react-bootstrap";
+import Button from "@restart/ui/esm/Button";
+
+function Me() {
+  return (
+    <div className="me">
+      <Container className="card">card</Container>
+    </div>
+  );
+}
+
+export default Me;
